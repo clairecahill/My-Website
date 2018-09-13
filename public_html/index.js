8873f12cf2485855a4fb5d604f0eb0a1f766ecc7
clairecahill <clairecahill@users.noreply.github.com>
2018-09-12 23:12:03 -0700

    First draft of website

---
+++ public_html/index.js
@@ -0,0 +1,9 @@
+function openNav(){
+
+   document.getElementById("side-nav-bar").style.width = "200px";
+}
+
+function closeNav(){
+
+   document.getElementById("side-nav-bar").style.width = "0";
+}
